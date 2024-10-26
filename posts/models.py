fd591ce0bbc0ee1036be0c131fa6d3b4762c43ae
ruhy <ruhollahmozafari@gmail.com>
2024-10-26 18:31:36 +0330

    create post part

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,31 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from accounts.serializers import User
+
+
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User, related_name = "posts", on_delete=models.CASCADE)
+    title = models.CharField(max_length=200,blank=False, null=False)
+    text = models.TextField(max_length=1000,blank=True,null=True)
+    is_public = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+
+
+class PostFile(models.Model):
+     post = models.ForeignKey(Post,related_name="post_files", on_delete=models.CASCADE)
+     image = models.ImageField(upload_to='image_file/post_image/',blank=True,null=True)
+     video = models.FileField(upload_to='image_file/post_video/',blank=True,null=True)
+     caption = models.TextField(max_length=500, blank=True, null=True)
+     file = models.FileField(upload_to='image_file/post_file/',blank=True,null=True)
+     created_at = models.DateTimeField(auto_now_add=True)
+     updated_at = models.DateTimeField(auto_now=True)
+
+     class Meta:
+         ordering = ['-created_at']
---
+++ posts/serializers.py
@@ -0,0 +1,80 @@
+from rest_framework import serializers
+from posts.models import Post, PostFile
+
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = '__all__'
+        read_only_fields = ['created_at', 'updated_at']
+
+    def validate(self, attrs):
+        text = attrs.get('text')
+
+        if len(text) > 1000:
+            raise serializers.ValidationError("text could not longer than 1000 characters")
+
+        return attrs
+
+
+    '''move def CRUD to views'''
+    # def create(self, validated_data):
+    #     post = Post.objects.create(**validated_data)
+    #     post.auther.profile.save()
+    #     return post
+    #
+    # def update(self, instance, validated_data):
+    #     instance.title = validated_data.get('title', instance.title)
+    #     instance.text = validated_data.get('text', instance.text)
+    #     instance.is_public = validated_data.get('is_public', instance.is_public)
+    #     instance.save()
+    #     return instance
+    #
+    # def delete(self, instance):
+    #     if not self.context['request'].user.is_authenticated:
+    #         raise serializers.ValidationError("You do not have permission to delete this post.")
+    #     instance.delete()
+    #     return {'detail': 'Post deleted '}
+
+
+
+
+class PostFileSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField(required=False)
+    video = serializers.FileField(required=False)
+    file = serializers.FileField(required=False)
+
+    class Meta:
+        model = PostFile
+        fields = '__all__'
+        read_only_fields = ['created_at', 'updated_at']
+
+
+    def validate(self, attrs):
+        caption = attrs.get('caption')
+
+        if len(caption) > 500:
+            raise serializers.ValidationError("caption could not longer than 500 characters")
+
+        return attrs
+
+
+    '''move def CRUD to views'''
+    # def create(self, validated_data):
+    #     postfile = PostFile.objects.create(**validated_data)
+    #     return postfile
+    #
+    # def update(self, instance, validated_data):
+    #     instance.image = validated_data.get('image', instance.image)
+    #     instance.video = validated_data.get('video', instance.video)
+    #     instance.file = validated_data.get('file', instance.file)
+    #     instance.caption = validated_data.get('caption', instance.caption)
+    #     instance.save()
+    #     return instance
+    #
+    # def delete(self, instance):
+    #     if not self.context['request'].user.is_authenticated:
+    #         raise serializers.ValidationError("You do not have permission to delete this file.")
+    #     instance.delete()
+    #     return {'detail': 'File deleted '}
