92ab72aa29d1b0330a01d1a1377c283aadf91564
ruhy <ruhollahmozafari@gmail.com>
2024-11-26 14:26:20 +0330

    add FriendShipList

--- connections/admin.py
+++ connections/admin.py
@@ -1,11 +1,7 @@
 from django.contrib import admin
-
-from accounts import serializers
 from connections.models import Connection
 
-
 class ConnectionAdmin(admin.ModelAdmin):
-
     actions = None
     list_display = ('request_sender', 'request_receiver', 'status', 'created_at')
     search_fields = ('request_sender', 'request_receiver')
--- connections/urls.py
+++ connections/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from .views import (
     RequestView,
     RequestListView,
-    RequestStatusView
+    RequestStatusView,
+    FriendShipListView
 )
 
 urlpatterns = [
     path('Request/', RequestView.as_view(), name='request'),
     path('Request_list/', RequestListView.as_view(), name='request_list'),
     path('Request_status/', RequestStatusView.as_view(), name='request_status'),
+    path('FriendShip_list/', FriendShipListView.as_view(), name='friend_list'),
 ]
