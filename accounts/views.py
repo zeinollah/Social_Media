9564b4e11c38bb21f4df877e4664ed94c71bab62
ruhy <ruhollahmozafari@gmail.com>
2024-12-18 19:01:31 +0330

    change accounts app

--- accounts/views.py
+++ accounts/views.py
@@ -1,26 +1,3 @@
-from django.contrib.auth import get_user_model
-from rest_framework import viewsets
-from rest_framework.permissions import AllowAny, IsAuthenticated
-from accounts.models import Profile
-from accounts.serializers import UserSerializer, ProfileSerializer
+from django.shortcuts import render
 
-
-
-User = get_user_model()
-class UserAPIView(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = [AllowAny]
-    search_fields = ['first_name', 'last_name', 'username', ]
-
-
-class ProfileAPIView(viewsets.ModelViewSet):
-    queryset = Profile.objects.all()
-    serializer_class = ProfileSerializer
-    permission_classes = [IsAuthenticated]
-    search_fields = ['username', 'email', 'phone_number' ]
-
-    #TODO: write save ip_address def .
-
-
-
+# Create your views here.
--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from accounts.serializers import User
+from django.contrib.auth.models import User
 
 
 
